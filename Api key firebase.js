ef66541b7e0a24ea79564bcf3a40f4718f894d38
sundarapandiyan <111949963+Sundarapandiyan504@users.noreply.github.com>
2023-10-26 07:16:41 +0530

    Create Api key firebase.js

---
+++ Api key firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB636OnkckBPUByyXs7ZqnoZWSlGze6v_s",
+  authDomain: "courseo-95c01.firebaseapp.com",
+  databaseURL: "https://courseo-95c01-default-rtdb.firebaseio.com",
+  projectId: "courseo-95c01",
+  storageBucket: "courseo-95c01.appspot.com",
+  messagingSenderId: "277852742342",
+  appId: "1:277852742342:web:f40fe6a8ed000b98b4edc7"
+};
